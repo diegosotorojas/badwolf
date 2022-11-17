9093f5f8ace3fa470bee52c22aa4d4146d4cfdbc
diegoasm12 <102439978+diegoasm12@users.noreply.github.com>
2022-11-17 10:35:58 -0300

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// CONFIGURACION DE GOOGLE
-$clientID = '1081847527558-lr1h7970dq3ojaqjqevusu43rvu0slj3.apps.googleusercontent.com';
-$clientSecret = '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';
-$redirectUri = 'http://diego.com.es/index.php';
-
